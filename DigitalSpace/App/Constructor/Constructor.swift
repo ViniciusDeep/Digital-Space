c237c468a2ea6d0742de071ddb276b13c7cd016e
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2019-08-30 22:37:39 -0300

    Init projet with constructor

---
+++ DigitalSpace/App/Constructor/Constructor.swift
@@ -0,0 +1,31 @@
+//
+//  Constructor.swift
+//  DigitalSpace
+//
+//  Created by Vinicius Mangueira on 30/08/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+protocol ConfigurableConstructor {
+    mutating func setupConstructor()
+}
+
+struct Constructor: ConfigurableConstructor {
+    var window: UIWindow?
+    
+    init() {
+        setupConstructor()
+    }
+    
+    mutating func setupConstructor() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+}
+
+
