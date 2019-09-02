1cc81ec5eaef0a0d313b8b7ade42fbb8bbe29608
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2019-09-02 07:16:03 -0300

    Update load image

---
+++ DigitalSpace/App/Features/Favorites/View/FavoritesViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesViewCell.swift
+//  DigitalSpace
+//
+//  Created by Vinicius Mangueira on 01/09/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import Foundation
